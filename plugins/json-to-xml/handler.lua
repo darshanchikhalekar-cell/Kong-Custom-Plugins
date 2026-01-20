1fd3e76a0905b263f86b410903162fdc7138d037
darshanchikhalekar-cell <darshan.chikhalekar@alliedglobetech.com>
2026-01-20 16:20:32 +0530

    Update handler.lua

---
+++ plugins/json-to-xml/handler.lua
@@ -0,0 +1,39 @@
+local cjson = require "cjson.safe"
+
+local Plugin = {
+  PRIORITY = 1000,
+  VERSION = "1.0.0"
+}
+
+-- Convert JSON table to XML
+local function table_to_xml(tbl, root)
+  local xml = "<" .. root .. ">"
+  for k, v in pairs(tbl) do
+    xml = xml .. "<" .. k .. ">" .. tostring(v) .. "</" .. k .. ">"
+  end
+  xml = xml .. "</" .. root .. ">"
+  return xml
+end
+
+function Plugin:access(conf)
+  local body = kong.request.get_raw_body()
+
+  if not body then
+    return
+  end
+
+  local json_tbl = cjson.decode(body)
+
+  if not json_tbl then
+    return kong.response.exit(400, {
+      message = "Invalid JSON body"
+    })
+  end
+
+  local xml_body = table_to_xml(json_tbl, conf.root_tag)
+
+  kong.service.request.set_header("Content-Type", "application/xml")
+  kong.service.request.set_raw_body(xml_body)
+end
+
+return Plugin
